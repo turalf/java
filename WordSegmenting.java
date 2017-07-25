e2334bf1a599260e2c2cd97884c2d341fedcb60f
Tural Farhadov <tfarhadov@lab.simbrella.com>
2017-07-25 18:13:37 +0400

    used array instead of hashmap in parent matching

--- WordSegmenting.java
+++ WordSegmenting.java
@@ -18,7 +18,8 @@ public class WordSegmenting{
 
 	public static String segmentIntoWords(String input){
 		List<Integer> breakPoints = new LinkedList<Integer>(Arrays.asList(0));
-		HashMap<Integer,Integer> parentMap = new HashMap<Integer,Integer>();
+		int[] parentMap = new int[input.length() + 1];
+		Arrays.fill(parentMap,-1);
 
 		for(int i=1;i<=input.length();i++){
 			int listSize = breakPoints.size();
@@ -28,25 +29,25 @@ public class WordSegmenting{
 				int startIndex = (Integer) iterator.next();		
 				if(_dictionary.contains(input.substring(startIndex,i))){
 					breakPoints.add(i);
-					parentMap.put(i,startIndex);
+					parentMap[i] = startIndex;
 					break;
 				}
 			}
 		}
 
 		//generating a segmented string
-		int endIndex = parentMap.containsKey(input.length()) ? (input.length()) : -1;
+		int endIndex = input.length();
 		int startIndex = -1;
 
 		System.out.println(breakPoints);
-		System.out.println(parentMap);
+		for(int i  : parentMap) if(i != -1) System.out.print(i + "->" + parentMap[i]+";");
 
 		if(endIndex == -1) return null;
 
 		StringBuilder sb = new StringBuilder();
 		
 		while(endIndex != 0){
-			startIndex = parentMap.get(endIndex);
+			startIndex = parentMap[endIndex];
 			sb.insert(0, input.substring(startIndex, endIndex) + " ");
 			endIndex = startIndex;
 		}	
